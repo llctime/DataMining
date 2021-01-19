e9085b5a2c3f206807c17871c4dd506c8b8f65dd
llctime <1046998331@qq.com>
2021-01-19 22:24:33 +0800

    update

--- Pipeline.py
+++ Pipeline.py
@@ -14,8 +14,11 @@ MISSING_VALUES = 'NaN'    # 原始数据缺失值
 
 ANALYSIS_FILEPATH = 'D:\\Dev\\Projects\\DataMining\\天池贷款违约预测训练数据字段分析.csv'
 TARGET = 'isDefault'    # 标签
+import pandas as pd
+TRAIN_LABEL = pd.DataFrame()
 ALGORITHMS = {
-    'RFC': __import__('sklearn.ensemble', globals={}, locals={}, fromlist=['ensemble']).RandomForestClassifier
+    'RFC': __import__('sklearn.ensemble', globals={}, locals={}, fromlist=['ensemble']).RandomForestClassifier,
+    'GBDT': __import__('sklearn.ensemble', globals={}, locals={}, fromlist=['ensemble']).GradientBoostingClassifier
 }    # 算法列表
 
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -270,110 +273,47 @@ class VarianceSelection(BaseEstimator, TransformerMixin):
         print('方差选择后的数据：\n', var_sel_data.head(10)) if SHOW_LOG == True else ()
         return var_sel_data
 
-def Main():
+class ChisquareSelection(BaseEstimator, TransformerMixin):
+    # 卡方检验特征选择
+    def __init__(self):
+        pass
 
-    # 导入原始数据
-    ori_data = readFileToDataFrame(TRAIN_DATA_FILEPATH, ENCODING, MISSING_VALUES)
-    print('原始数据：\n',ori_data.head(10)) if SHOW_LOG == True else ()
+    def fit(self, X, y=None):
+        return self
 
-    # 初步判断分类、删除字段
-    makeAnalysisFile(ANALYSIS_FILEPATH, ori_data)
-
-    from sklearn.pipeline import Pipeline
-    pipeline = Pipeline([('del_Columns', ColumnDeletion()),
-                           ('impute_miss', MissingImputation()),
-                           ('digi_string', Digitization()),
-                           ('bin', Binaryzation()),
-                           ('scaler', Standardization()),
-                           ('onehot', OneHotEncoding()),
-                           ('cal_features', FeatureCalculation()),
-                           ('poly_features', FeaturePolynomialization()),
-                           ('sel_var', VarianceSelection())])
-    var_sel_data = pipeline.fit_transform(ori_data)
-
-    # 2. 卡方检验特征选择，注，运行时间较长，建议第一次跑完之后把筛选后的特征列保存，之后不再跑此过程
-    chi2_sel_data = var_sel_data.copy()
-    CHI2_FEATURES = ['loanAmnt', 'term', 'interestRate', 'installment', 'grade', 'subGrade',
-                     'employmentTitle', 'homeOwnership', 'annualIncome',
-                     'verificationStatus', 'dti', 'delinquency_2years', 'openAcc', 'pubRec',
-                     'pubRecBankruptcies', 'revolUtil', 'initialListStatus',
-                     'applicationType', 'n1', 'n2', 'n3', 'n5', 'n7', 'n9', 'n10', 'n14',
-                     'onehot_0', 'onehot_1', 'onehot_2', 'onehot_3', 'onehot_4', 'onehot_5',
-                     'onehot_6', 'onehot_8', 'onehot_9', 'onehot_10', 'onehot_12',
-                     'onehot_14', 'onehot_16', 'onehot_17', 'onehot_19', 'onehot_20',
-                     'onehot_21', 'onehot_22', 'onehot_25', 'onehot_26', 'onehot_27',
-                     'onehot_29', 'onehot_31', 'onehot_32', 'onehot_33', 'onehot_34',
-                     'onehot_35', 'onehot_36', 'onehot_38', 'onehot_39', 'onehot_40',
-                     'onehot_41', 'onehot_43', 'onehot_44', 'onehot_46', 'onehot_47',
-                     'onehot_48', 'onehot_49', 'onehot_50', 'onehot_51', 'onehot_52',
-                     'onehot_53', 'onehot_54', 'onehot_55', 'onehot_56', 'onehot_58',
-                     'onehot_59', 'onehot_61']
-    chi2_sel_data = chi2_sel_data[CHI2_FEATURES]
-    print('卡方选择后的特征：\n', CHI2_FEATURES) if SHOW_LOG == True else ()
-    print('卡方选择后的数据：\n', chi2_sel_data.head(10)) if SHOW_LOG == True else ()
-    # from sklearn.feature_selection import mutual_info_classif as MIC
-    # mic = MIC(var_sel_data, ori_data[TARGET])    # 互信息法验证特征与标签的相关性（线性和非线性），0为相互独立，1为相关
-    # CHI_K = mic.shape[0] - (mic <= 0).sum()    # k值由特征总数减去p>0.05的特征数得到
-    # print('卡方选择的k值：\n', CHI_K) if SHOW_LOG == True else ()
-    # if CHI_K > 0:
-    #     from sklearn.feature_selection import SelectKBest
-    #     from sklearn.feature_selection import chi2
-    #     selectkbest = SelectKBest(chi2, k=CHI_K)
-    #     chi2_sel_data = pd.DataFrame(data=selectkbest.fit_transform(var_sel_data, ori_data[TARGET]), columns=(var_sel_data.columns[item] for item in selectkbest.get_support(indices=True)))
-    # else:
-    #     print('无需进行卡方检验选择！') if SHOW_LOG == True else ()
-    # print('卡方选择后的特征：\n', chi2_sel_data.columns) if SHOW_LOG == True else ()
-    # print('卡方选择后的数据：\n', chi2_sel_data.head(10)) if SHOW_LOG == True else ()
-
-    # 嵌入法和包装法精筛特征，但参数选择较为依赖启发式策略，故需根据结果好坏决定是否选用以下的选择方法
-
-    # 嵌入法：利用算法模型真实验证特征相关性，得到feature_importance（适合特征经过粗筛后，寻找最为相关的特征）注，运行时间较长
-    EMB_ALGORITHM = 'RFC'
-    EMB_N_ESTIMATORS = 10
-    # EMB_PARTITIONS = 10
-    # EMB_CV = 5
-    # emb_threshold = plotEmbedded(EMB_ALGORITHM, EMB_N_ESTIMATORS, chi2_sel_data, ori_data[TARGET], EMB_PARTITIONS, EMB_CV)
-    # print('嵌入法的阈值为：\n', emb_threshold) if SHOW_LOG == True else ()
-    emb_threshold = 0.07125063715469972
-    print('嵌入法的阈值为：\n', emb_threshold) if SHOW_LOG == True else ()
-    emb_sel_data = chi2_sel_data.copy()
-    print('嵌入法选择后的数据：\n', emb_sel_data) if SHOW_LOG == True else ()
-    # from sklearn.feature_selection import SelectFromModel
-    # emb_estimator = ALGORITHMS.get(EMB_ALGORITHM)(n_estimators=EMB_N_ESTIMATORS, random_state=0)
-    # selectfrommodel = SelectFromModel(emb_estimator, threshold=emb_threshold)
-    # emb_sel_data = pd.DataFrame(data=selectfrommodel.fit_transform(chi2_sel_data, ori_data[TARGET]), columns=(chi2_sel_data.columns[item] for item in selectfrommodel.get_support(indices=True)))
-    # print('嵌入法选择后的数据：\n', emb_sel_data) if SHOW_LOG == True else ()
-
-    # 包装法：黑盒选取特征子集，并有专用的算法RFE进行筛选，不需要学习超参数（即阈值）
-    WRAP_ALGORITHM = 'RFC'
-    WRAP_N_ESTIMATORS = 10
-    WRAP_STEP = 5    # RFE每次迭代时筛掉多少个特征
-    WRAP_CV = 3
-    # wrap_n_features_to_select = plotWrapper(WRAP_ALGORITHM, WRAP_N_ESTIMATORS, chi2_sel_data, ori_data[TARGET], WRAP_STEP, WRAP_CV)
-    wrap_sel_data = emb_sel_data.copy()
-    print('包装法选择后的数据：\n', wrap_sel_data) if SHOW_LOG == True else ()
-    # from sklearn.feature_selection import RFE
-    # wrap_estimator = ALGORITHMS.get(WRAP_ALGORITHM)(n_estimators=WRAP_N_ESTIMATORS, random_state=0)
-    # rfe = RFE(wrap_estimator, wrap_n_features_to_select, step=WRAP_STEP)
-    # print('包装法特征重要性排名：\n', rfe.fit(chi2_sel_data, ori_data[TARGET]).ranking_) if SHOW_LOG == True else ()
-    # wrap_sel_data = pd.DataFrame(data=rfe.fit_transform(chi2_sel_data, ori_data[TARGET]), columns=(chi2_sel_data.columns[item] for item in rfe.get_support(indices=True)))
-    # print('包装法选择后的数据：\n', wrap_sel_data) if SHOW_LOG == True else ()
-
-    # PCA降维
-    dec_pca_data = wrap_sel_data.copy()
-    # plotPCA(dec_pca_data)    # 找拐点的特征数作为n_components，如果曲线很平滑，没有明显拐点，就不使用pca
-    # N_COMPONENTS = 20
-    # from sklearn.decomposition import PCA
-    # pca = PCA(n_components=N_COMPONENTS)
-    # dec_pca_data = pd.DataFrame(data=pca.fit_transform(dec_pca_data), columns=('pca_'+ str(item) for item in range(N_COMPONENTS)))
-    print('PCA降维后的数据：\n', dec_pca_data) if SHOW_LOG == True else ()
-
-
-
-
-    # ------------------------------------------------------------------- #
-
-
+    def transform(self, X, y=None):
+        chi2_sel_data = X.copy()
+        CHI2_FEATURES = ['loanAmnt', 'term', 'interestRate', 'installment', 'grade', 'subGrade',
+                         'employmentTitle', 'homeOwnership', 'annualIncome',
+                         'verificationStatus', 'dti', 'delinquency_2years', 'openAcc', 'pubRec',
+                         'pubRecBankruptcies', 'revolUtil', 'initialListStatus',
+                         'applicationType', 'n1', 'n2', 'n3', 'n5', 'n7', 'n9', 'n10', 'n14',
+                         'onehot_0', 'onehot_1', 'onehot_2', 'onehot_3', 'onehot_4', 'onehot_5',
+                         'onehot_6', 'onehot_8', 'onehot_9', 'onehot_10', 'onehot_12',
+                         'onehot_14', 'onehot_16', 'onehot_17', 'onehot_19', 'onehot_20',
+                         'onehot_21', 'onehot_22', 'onehot_25', 'onehot_26', 'onehot_27',
+                         'onehot_29', 'onehot_31', 'onehot_32', 'onehot_33', 'onehot_34',
+                         'onehot_35', 'onehot_36', 'onehot_38', 'onehot_39', 'onehot_40',
+                         'onehot_41', 'onehot_43', 'onehot_44', 'onehot_46', 'onehot_47',
+                         'onehot_48', 'onehot_49', 'onehot_50', 'onehot_51', 'onehot_52',
+                         'onehot_53', 'onehot_54', 'onehot_55', 'onehot_56', 'onehot_58',
+                         'onehot_59', 'onehot_61']
+        chi2_sel_data = X[CHI2_FEATURES]
+        # 注，运行时间较长，建议第一次跑完之后把筛选后的特征列保存，之后不再跑此过程
+        # from sklearn.feature_selection import mutual_info_classif as MIC
+        # mic = MIC(X, TRAIN_LABEL)    # 互信息法验证特征与标签的相关性（线性和非线性），0为相互独立，1为相关
+        # CHI_K = mic.shape[0] - (mic <= 0).sum()    # k值由特征总数减去p>0.05的特征数得到
+        # print('卡方选择的k值：\n', CHI_K) if SHOW_LOG == True else ()
+        # if CHI_K > 0:
+        #     from sklearn.feature_selection import SelectKBest
+        #     from sklearn.feature_selection import chi2
+        #     selectkbest = SelectKBest(chi2, k=CHI_K)
+        #     chi2_sel_data = pd.DataFrame(data=selectkbest.fit_transform(X, TRAIN_LABEL), columns=(X.columns[item] for item in selectkbest.get_support(indices=True)))
+        # else:
+        #     print('无需进行卡方检验选择！') if SHOW_LOG == True else ()
+        # print('卡方选择后的特征：\n', CHI2_FEATURES) if SHOW_LOG == True else ()
+        # print('卡方选择后的数据：\n', chi2_sel_data.head(10)) if SHOW_LOG == True else ()
+        return chi2_sel_data
 
 def plotEmbedded(algorithm, n_estimators, X, y, partitions, cv):
     # time warning #
@@ -440,6 +380,83 @@ def plotPCA(X):
     plt.xlabel("number of components after dimension reduction")
     plt.ylabel("cumulative explained variance ratio")
     plt.show()
+
+class PCADecomposition(BaseEstimator, TransformerMixin):
+    # PCA降维
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        dec_pca_data = X.copy()
+        plotPCA(dec_pca_data)    # 找拐点的特征数作为n_components，如果曲线很平滑，没有明显拐点，就不使用pca
+        # N_COMPONENTS = 20
+        # from sklearn.decomposition import PCA
+        # pca = PCA(n_components=N_COMPONENTS)
+        # dec_pca_data = pd.DataFrame(data=pca.fit_transform(X), columns=('pca_'+ str(item) for item in range(N_COMPONENTS)))
+        # print('PCA降维后的数据：\n', dec_pca_data) if SHOW_LOG == True else ()
+        return dec_pca_data
+
+def Main():
+
+    # 导入原始数据
+    ori_data = readFileToDataFrame(TRAIN_DATA_FILEPATH, ENCODING, MISSING_VALUES)
+    TRAIN_LABEL = ori_data[TARGET]
+    print('原始数据：\n',ori_data.head(10)) if SHOW_LOG == True else ()
+
+    # 初步判断分类、删除字段
+    makeAnalysisFile(ANALYSIS_FILEPATH, ori_data)
+
+    from sklearn.pipeline import Pipeline
+    pipeline = Pipeline([('del_Columns', ColumnDeletion()),
+                           ('impute_miss', MissingImputation()),
+                           ('digi_string', Digitization()),
+                           ('bin', Binaryzation()),
+                           ('scaler', Standardization()),
+                           ('onehot', OneHotEncoding()),
+                           ('cal_features', FeatureCalculation()),
+                           ('poly_features', FeaturePolynomialization()),
+                           ('sel_var', VarianceSelection()),
+                           ('chi2', ChisquareSelection())])
+    chi2_sel_data = pipeline.fit_transform(ori_data)
+
+    # 嵌入法和包装法精筛特征，但参数选择较为依赖启发式策略，故需根据结果好坏决定是否选用以下的选择方法
+
+    # # 嵌入法：利用算法模型真实验证特征相关性，得到feature_importance（适合特征经过粗筛后，寻找最为相关的特征）注，运行时间较长
+    # EMB_ALGORITHM = 'RFC'
+    # EMB_N_ESTIMATORS = 10
+    # # EMB_PARTITIONS = 10
+    # # EMB_CV = 5
+    # # emb_threshold = plotEmbedded(EMB_ALGORITHM, EMB_N_ESTIMATORS, chi2_sel_data, ori_data[TARGET], EMB_PARTITIONS, EMB_CV)
+    # # print('嵌入法的阈值为：\n', emb_threshold) if SHOW_LOG == True else ()
+    # emb_threshold = 0.07125063715469972
+    # print('嵌入法的阈值为：\n', emb_threshold) if SHOW_LOG == True else ()
+    # emb_sel_data = chi2_sel_data.copy()
+    # print('嵌入法选择后的数据：\n', emb_sel_data) if SHOW_LOG == True else ()
+    # # from sklearn.feature_selection import SelectFromModel
+    # # emb_estimator = ALGORITHMS.get(EMB_ALGORITHM)(n_estimators=EMB_N_ESTIMATORS, random_state=0)
+    # # selectfrommodel = SelectFromModel(emb_estimator, threshold=emb_threshold)
+    # # emb_sel_data = pd.DataFrame(data=selectfrommodel.fit_transform(chi2_sel_data, ori_data[TARGET]), columns=(chi2_sel_data.columns[item] for item in selectfrommodel.get_support(indices=True)))
+    # # print('嵌入法选择后的数据：\n', emb_sel_data) if SHOW_LOG == True else ()
+    #
+    # # 包装法：黑盒选取特征子集，并有专用的算法RFE进行筛选，不需要学习超参数（即阈值）
+    # WRAP_ALGORITHM = 'RFC'
+    # WRAP_N_ESTIMATORS = 10
+    # WRAP_STEP = 5    # RFE每次迭代时筛掉多少个特征
+    # WRAP_CV = 3
+    # # wrap_n_features_to_select = plotWrapper(WRAP_ALGORITHM, WRAP_N_ESTIMATORS, chi2_sel_data, ori_data[TARGET], WRAP_STEP, WRAP_CV)
+    # wrap_sel_data = emb_sel_data.copy()
+    # print('包装法选择后的数据：\n', wrap_sel_data) if SHOW_LOG == True else ()
+    # # from sklearn.feature_selection import RFE
+    # # wrap_estimator = ALGORITHMS.get(WRAP_ALGORITHM)(n_estimators=WRAP_N_ESTIMATORS, random_state=0)
+    # # rfe = RFE(wrap_estimator, wrap_n_features_to_select, step=WRAP_STEP)
+    # # print('包装法特征重要性排名：\n', rfe.fit(chi2_sel_data, ori_data[TARGET]).ranking_) if SHOW_LOG == True else ()
+    # # wrap_sel_data = pd.DataFrame(data=rfe.fit_transform(chi2_sel_data, ori_data[TARGET]), columns=(chi2_sel_data.columns[item] for item in rfe.get_support(indices=True)))
+    # # print('包装法选择后的数据：\n', wrap_sel_data) if SHOW_LOG == True else ()
+
+    # ------------------------------------------------------------------- #
 
 
 
